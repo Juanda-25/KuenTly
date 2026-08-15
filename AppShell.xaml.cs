f4297ca9ed9a295b30b3809f3eb83c9c9e6f5872
Juanda <Juangueva@gmail.com>
2026-08-15 18:25:12 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KuenTly
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/Inicio/InicioPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace KuenTly.Views.Inicio
+{
+    public partial class InicioPage : ContentPage
+    {
+        int count = 0;
+
+        public InicioPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
